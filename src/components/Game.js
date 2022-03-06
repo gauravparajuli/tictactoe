6613c5689805017970d9ecef7c9630963a596103
gaurav parajuli <gparajuli.1997@gmail.com>
2022-03-06 10:20:49 +0545

    game is now playable

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,13 +2,18 @@ import React, { useState } from 'react'
 import Board from './Board'
 import { calculateWinner } from '../helpers'
 
+const style = {
+    width: '200px',
+    margin: '20px auto'
+}
+
 const Game = () => {
 
     const [board, setBoard] = useState(Array(9).fill(null))
     const [xIsNext, setXisNext] = useState(true)
     const winner = calculateWinner(board)
 
-    const handleClick = (i) => {
+    const handleClick = i => {
         const boardDuplicate = [...board]
         // if user clicks on occupied square or if game is won, just return
         if (winner || boardDuplicate[i]) return null
@@ -22,12 +27,18 @@ const Game = () => {
 
     }
 
-    const renderMoves = () => {
-
-    }
+    const renderMoves = () => (
+        <button onClick={() => setBoard(Array(9).fill(null))}>Start New Game</button>
+    )
 
     return (
-        <Board squares={board} onClick={handleClick} />
+        <>
+            <Board squares={board} onClick={handleClick} />
+            <div style={style}>
+                <p>{winner ? 'Winner is: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
+                {renderMoves()}
+            </div>
+        </>
     )
 }
 
