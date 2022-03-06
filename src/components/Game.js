1a06e7baa7fd1005b8522cab9d4caf9594027e35
gaurav parajuli <gparajuli.1997@gmail.com>
2022-03-06 09:47:28 +0545

    implmented handle click function

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,8 +8,14 @@ const Game = () => {
     const [xIsNext, setXisNext] = useState(true)
     const winner = calculateWinner(board)
 
-    const handleClick = () => {
-
+    const handleClick = (i) => {
+        const boardDuplicate = [...board]
+        // if user clicks on occupied square or if game is won, just return
+        if (winner || boardDuplicate[i]) return null
+        // put 'x' or 'o' in the clicked square
+        boardDuplicate[i] = xIsNext ? 'X' : 'O'
+        setBoard(boardDuplicate)
+        setXisNext(!xIsNext) // its another player turn now
     }
 
     const jumpTo = () => {
